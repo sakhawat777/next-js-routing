5547fb5a2e360c614bcd38124f816bb7c51c4056
sakhawat777 <biplobbms@gmail.com>
2022-04-13 10:16:43 +0600

    Index Routes Done

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
 
 const index = () => {
-	return <div></div>;
+	return (
+		<div>
+			{/* Index Routes */}
+			<ul>
+				<li>
+					<Link href='/'>Home</Link>
+				</li>
+				<li>
+					<Link href='/contact'>Contact</Link>
+				</li>
+
+				<li>
+					<Link href='/about'>About</Link>
+				</li>
+			</ul>
+			<h1>This is a Home Page</h1>
+		</div>
+	);
 };
 
 export default index;
